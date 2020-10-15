56939a211d419e494e1d363a201e8e353c183c21
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-10-16 00:00:37 +0700

    6

---
+++ Sotaro/SotaroWebsite/Models/product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SotaroWebsite.Models
+{
+    public class product
+    {
+        internal string Id;
+
+        public string ID { set; get; }
+       
+        public string Maker { get; set; }
+        
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        
+        
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+
+        public string Desrciption { get; set; }
+
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<product>(this);
+        
+    }
+}
---
+++ Sotaro/SotaroWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SotaroWebsite.Models;
+
+namespace SotaroWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+
+        public IEnumerable<product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;  
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+
+    }
+}
